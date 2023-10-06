97911edba4f036aca9e3959b1ef567582a41b739
Hermann EHO <hermannleboss@gmail.com>
2023-10-06 07:02:59 +0000

    feat: design of output ok

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,36 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useFetch } from '@vueuse/core'
+
+const url = 'http://localhost:3000'
+const { data, isFetching, isFinished, statusCode, error } = useFetch(url)
+console.log('data', data.value)
+</script>
 
 <template>
-  Normal content
+  <div class="overflow-x-auto">
+    <table class="table">
+      <tr>
+        <th>Data</th>
+        <td>{{ data }}</td>
+      </tr>
+      <tr>
+        <th>IsFetching</th>
+        <td>{{ isFetching }}</td>
+      </tr>
+      <tr>
+        <th>IsFinished</th>
+        <td>{{ isFinished }}</td>
+      </tr>
+      <tr>
+        <th>Status Code</th>
+        <td>{{ statusCode }}</td>
+      </tr>
+      <tr>
+        <th>Error</th>
+        <td>{{ error }}</td>
+      </tr>
+    </table>
+  </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
