376ca60c539f384ca24349f8d92618a25064f511
Hermann EHO <hermannleboss@gmail.com>
2023-10-06 07:51:17 +0000

    feat: create comparison component

---
+++ src/Comparaison.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="border">
+    <div class="p-4">
+      <table class="table table-zebra">
+        <tbody>
+        <tr>
+          <th>URL</th>
+          <td>{{ url }}</td>
+        </tr>
+        <tr>
+          <th>Test</th>
+          <td>Test not existing url</td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="p-4">
+      <UseFetchOutput :url="url" />
+    </div>
+    <div class="p-4">
+      <SimpleFetchOutput :url="url" />
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+import UseFetchOutput from "@/components/UseFetchOutput.vue";
+import SimpleFetchOutput from "@/components/SimpleFetchOutput.vue";
+
+const props = defineProps({
+  url: {
+    type: String
+  }
+})
+</script>
