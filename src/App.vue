51bc316c7f69ebf2b003bdf4466110207ee6abca
Hermann EHO <hermannleboss@gmail.com>
2023-10-06 07:56:11 +0000

    feat: new test

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,19 @@
 import Comparaison from '@/Comparaison.vue'
 
 const url = 'http://localhost:3000'
-const urlWithError = 'http://localhostds:3000'
+const urlWithError = 'http://urlWithError:3000'
 
-const table = [{ url: url }, { url: urlWithError }]
+const table = [
+  { url: url, test: 'Simple 200 page' },
+  { url: urlWithError, test: 'Network Error' },
+  { url: url + '/404', test: 'Simple 404 page' },
+  { url: url + '/500', test: 'Simple 500 page' }
+]
 </script>
 
 <template>
   <div class="flex">
-    <Comparaison :url="item.url" v-for="(item, i) in table" :key="i" />
+    <Comparaison :url="item.url" :test="item.test" v-for="(item, i) in table" :key="i" />
   </div>
 </template>
 
