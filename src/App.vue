4056a4682129a3c13151af041858b80e1a217550
Hermann EHO <hermannleboss@gmail.com>
2023-10-06 07:10:25 +0000

    feat: extract use fetch component

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,11 @@
 <script setup lang="ts">
-import { useFetch } from '@vueuse/core'
+import UseFetchOutput from '@/components/UseFetchOutput.vue'
 
 const url = 'http://localhost:3000'
-const { data, isFetching, isFinished, statusCode, error } = useFetch(url)
-console.log('data', data.value)
 </script>
 
 <template>
-  <div class="overflow-x-auto">
-    <table class="table">
-      <tr>
-        <th>Data</th>
-        <td>{{ data }}</td>
-      </tr>
-      <tr>
-        <th>IsFetching</th>
-        <td>{{ isFetching }}</td>
-      </tr>
-      <tr>
-        <th>IsFinished</th>
-        <td>{{ isFinished }}</td>
-      </tr>
-      <tr>
-        <th>Status Code</th>
-        <td>{{ statusCode }}</td>
-      </tr>
-      <tr>
-        <th>Error</th>
-        <td>{{ error }}</td>
-      </tr>
-    </table>
-  </div>
+  <UseFetchOutput :url="url" />
 </template>
 
 <style scoped></style>
---
+++ src/components/UseFetchOutput.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="overflow-x-auto">
+    <table class="table">
+      <tr>
+        <th>Data</th>
+        <td>{{ data }}</td>
+      </tr>
+      <tr>
+        <th>IsFetching</th>
+        <td>{{ isFetching }}</td>
+      </tr>
+      <tr>
+        <th>IsFinished</th>
+        <td>{{ isFinished }}</td>
+      </tr>
+      <tr>
+        <th>Status Code</th>
+        <td>{{ statusCode }}</td>
+      </tr>
+      <tr>
+        <th>Error</th>
+        <td>{{ error }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+<script lang="ts" setup>
+import { useFetch } from '@vueuse/core'
+
+const props = defineProps({
+  url: {
+    type: String
+  }
+})
+const { data, isFetching, isFinished, statusCode, error } = useFetch(props.url)
+const response = useFetch(props.url)
+//console.table(response)
+</script>
