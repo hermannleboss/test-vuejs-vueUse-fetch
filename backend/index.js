cfa6fe83e5e6f89e812704df117b8b6f76f68937
Hermann EHO <hermannleboss@gmail.com>
2023-10-06 06:09:54 +0000

    feat: create a 404 and 500 error

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,15 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+// Return a 404 response
+app.get('/404', (req, res)=>{
+    res.status(404).json({error: "We have a 404 Error"})
+})
+
+// Return a 500 error
+app.get('/500', (req, res)=>{
+    res.status(500).json({error: "We have a 500 Error"})
+})
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
